0dd0e92561a2d742156513cf409c2fd9e9556bb9
Stuart A. Roth <stuartaroth@gmail.com>
2018-12-26 13:32:37 -0800

    Changed to use ConfigServiceFactory statically

--- src/main/java/com/zoodeck/common/config/ConfigServiceFactory.java
+++ src/main/java/com/zoodeck/common/config/ConfigServiceFactory.java
@@ -3,14 +3,7 @@ package com.zoodeck.common.config;
 import java.util.Map;
 
 public class ConfigServiceFactory {
-
-    private Map<String, String> env;
-
-    public ConfigServiceFactory(Map<String, String> env) {
-        this.env = env;
-    }
-
-    public ConfigService getConfigService() {
+    public static ConfigService getConfigService(Map<String, String> env) {
         // todo add logic to figure out which ConfigService implementation to create
         return new LocalConfigService(env);
     }
